026edd1c5ffa323abf0c58a6c1a0a5a6a0405f02
Tejas Ghadge <ghadgetejas802@gmail.com>
2026-09-12 21:29:00 +0530

    History page UI Done. Every things work fine, except interview status.

--- frontend/src/pages/InterviewHistory.jsx
+++ frontend/src/pages/InterviewHistory.jsx
@@ -16,7 +16,7 @@ const InterviewHistory = () => {
 
                 console.log(result.data);
 
-                setInterviews(result.data);
+                setInterviews(result.data.interviews);
             }catch(err){
                 console.error(err);
             }
@@ -34,7 +34,7 @@ const InterviewHistory = () => {
                 className='mt-1 p-3 rounded-full bg-white shadow hover:shadow-md transition'><FaArrowLeft className='text-gray-600' /></button>
 
                 <div>
-                    <h1 className='text-3xl font-bold text-gray-800 flex-nowrap'>
+                    <h1 className='text-3xl font-bold text-gray-800'>
                         Interview History
                     </h1>
 
@@ -55,8 +55,66 @@ const InterviewHistory = () => {
                 </div>
             :
                 <div className='grid gap-6'>
-                    {interviews.map((item, index) => (
-                        <div key={index} className='bg-white p-6 rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 cursor-pointer border border-gray-100'>
+                    {interviews.map((item) => (
+                        <div key={item._id} 
+                        onClick={()=>navigate(`/report/${item._id}`)}
+                        className='bg-white p-6 rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 cursor-pointer border border-gray-100'>
+
+                            <div className='flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+
+                                <div className=''>
+                                    <h3 className='text-lg font-semibold text-gray-800'>
+                                        {item.role.charAt(0).toUpperCase() + item.role.slice(1)}
+                                    </h3>
+
+                                    <p className='text-gray-500 text-sm mt-1'>
+                                        {item.experience.charAt(0).toUpperCase() + item.experience.slice(1)} 
+                                        ⚫ 
+                                        {item.mode.charAt(0).toUpperCase() + item.mode.slice(1)}
+                                    </p>
+
+                                    <p className='text-sm text-gray-400 mt-2'>
+                                        {
+                                            new Date(item.createdAt).toLocaleDateString("en-IN", {
+                                                day: "2-digit",
+                                                month: "short",
+                                                year: "numeric",
+                                            })
+                                        }
+                                    </p>
+                                </div>
+
+
+                                <div className='flex items-center gap-6'>
+
+                                    {/* SCORE */}
+                                    <div className='text-right'>
+                                        <p className='text-xl font-bold text-emerald-600'>
+                                            {item.finalScore ?? 0}/10
+                                        </p>
+
+                                        <p className='text-xs text-gray-400'>
+                                            Overall Score
+                                        </p>
+                                    </div>
+
+
+                                    {/* STATUS BADGE */}
+                                    <span 
+                                        className={`px-4 py-1 rounded-full text-xs font-medium ${
+                                            item.status?.toLowerCase() === "completed"
+                                            ? "bg-emerald-100 text-emerald-700"
+                                            : "bg-yellow-100 text-yellow-700"
+                                        }`}
+                                    >
+                                        {
+                                            item.status.charAt(0).toUpperCase() + item.status.slice(1)
+                                        }
+                                    </span>
+
+                                </div>
+
+                            </div>
                             
                         </div>
                     ))
--- frontend/src/pages/InterviewReport.jsx
+++ frontend/src/pages/InterviewReport.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InterviewReport = () => {
   return (
-    <div>InterviewReport</div>
+    <div>Interview Report</div>
   )
 }
 
